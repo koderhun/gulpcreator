42d16ee1ae21e63b99308064569ddb3bd7abdcec
koderhun <example@example.com>
2026-06-08 14:56:29 +0300

    up version

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -1,34 +1,36 @@
-'use strict';
+'use strict'
 
-import { paths } from '../gulpfile.babel';
-import gulp from 'gulp';
-import gulpif from 'gulp-if';
-import rename from 'gulp-rename';
-import mincss from 'gulp-clean-css';
-import groupmedia from 'gulp-group-css-media-queries';
-import sourcemaps from 'gulp-sourcemaps';
-import plumber from 'gulp-plumber';
-import browsersync from 'browser-sync';
-import debug from 'gulp-debug';
-import yargs from 'yargs';
-import postcss from 'gulp-postcss';
+import {paths} from '../gulpfile.babel'
+import gulp from 'gulp'
+import gulpif from 'gulp-if'
+import rename from 'gulp-rename'
+import mincss from 'gulp-clean-css'
+import sourcemaps from 'gulp-sourcemaps'
+import plumber from 'gulp-plumber'
+import browsersync from 'browser-sync'
+import debug from 'gulp-debug'
+import yargs from 'yargs'
+import postcss from 'gulp-postcss'
 
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-const sass = gulpSass(dartSass);
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+const sass = gulpSass(dartSass)
 
 const argv = yargs.argv,
-  production = !!argv.production;
+  production = !!argv.production
 
 gulp.task('styles', () => {
   return gulp
     .src(paths.styles.src)
     .pipe(gulpif(!production, sourcemaps.init()))
     .pipe(plumber())
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(groupmedia())
+    .pipe(
+      sass
+        .sync({
+          outputStyle: 'expanded',
+        })
+        .on('error', sass.logError),
+    )
     .pipe(postcss())
     .pipe(
       gulpif(
@@ -69,5 +71,5 @@ gulp.task('styles', () => {
         title: 'CSS files',
       }),
     )
-    .pipe(browsersync.stream());
-});
+    .pipe(browsersync.stream())
+})
